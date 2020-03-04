670a9199442a443d4cc1591c63336fd64c981eae
Samet <47507568+sametkaplan98@users.noreply.github.com>
2020-03-04 21:13:45 +0300

    Update rect_detect.py

--- rect_detect.py
+++ rect_detect.py
@@ -12,16 +12,13 @@ img=imutils.resize(img,width=800)
 cv2.imshow('gri',img)
 
 _, threshold = cv2.threshold(img, 110, 255, cv2.THRESH_BINARY) #Piksel değerleri için belirli bir eşik (threshold) değeri için alt 0 ve üst 255 olacak Binary transformasyonu
-  
- 
-contours, hi= cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) #cv2 içinde bulunan Kontor bulmak için kullandığım fonksiyon. cv2.CHAIN_APPROX_NONE diyerek
-                                                                                #diyerek sadece 4 köşe noktayı elde ederiz (Amacımız da zaten dörtgen bulmak)
+contours, hi= cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) #cv2 içinde bulunan Kontor bulmak için kullandığım fonksiyon. cv2.CHAIN_APPROX_NONE kullanılarak
+                                                                                #sadece bütün kontor çizgisi yerine çizgilerin başlangıç ve bitiş koordinatları elde edilir.
 cv2.imshow('thresh',threshold) #Threshold uygulanmış resmin gösterimi
 for cnt in contours : 
   
     approx = cv2.approxPolyDP(cnt, 0.009 * cv2.arcLength(cnt, True), True)      # numpy array'i cinsinden kontorların alınması
     alan=cv2.contourArea(cnt) #Kontor alanı
-    
     if alan > 200 and alan < 3000 :       # Alan parametreleri, fotoğrafın çekildiği yüksekliğe göre değişiyor. 
                                           # Aynı yukseklikten cekilmiş olan fotograflar icin sabit degerler kullanilabilir.
         cevre= cv2.arcLength(cnt,True)    
@@ -29,7 +26,7 @@ for cnt in contours :
         x,y,w,h = cv2.boundingRect(cnt)
         fark=int(w-h)
         
-        if abs(fark) <100: # -- Elde edilen dörgenlerden kareye en yakın  çizdirmek için genişlik ve yükseklik değeri farkı belirliyorum.
+        if abs(fark) <100: # -- Elde edilen dörgenlerden kareye en yakın olanı çizdirmek için genişlik ve yükseklik değeri farkı eldesi.
          cv2.rectangle(img2,(x,y),(x+w,y+h),(0,255,0),2) # kontor dortgeni
          cv2.rectangle(img2,(x+int(w/4),y+int(h/4)) , (x+w-int(w/4), y+h-int(h/4)), (255,0,0), 1) #muhtemel harf bolgesi (2x2'lik posterde 1x1'lik harf bölgesi)
          #cv2.drawContours(img, [approx], 0, (0, 0, 255), 5) #İşlenmiş resimden elde edilen kontorların en başta import ettiğimiz işlenmemiş renkli fotoğraf üzerine çizilmesi
